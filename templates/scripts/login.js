387be2f920f82f987bdef44a88bcc208eb61ef63
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-10-10 21:26:08 +0200

    Entrega parcial Semana_1

--- templates/scripts/login.js
+++ templates/scripts/login.js
@@ -14,6 +14,27 @@ function check() {
     
     if (!checkText("Introduzca un nombre válido.", name, log)) boolean = false;
     if (!checkText("Introduzca una contraseña válida.", password, log)) boolean = false;
-    
+   
+    if (boolean && document.getElementById('keepSession').checked) {
+
+        var json = '{"name":"' + name.value +'","password":"' + password.value + '"}';
+
+        setCookie("keepSession", json, "432000");
+    }
+
     return boolean;
+}
+
+
+function setLogin() {
+
+    var text = getCookie("keepSession");
+    
+    if (text == "") return;
+
+    var json = JSON.parse(text);
+
+    document.getElementById('name').value = json.name;
+    document.getElementById('password').value = json.name;
+    document.getElementById('keepSession').checked = true;
 }

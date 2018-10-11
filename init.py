66198b851eff2c8330e5676160c40bbfa2cbb204
AlbertoMorenoB <alberto.morenob@estudiante.uam.es>
2018-10-11 10:38:37 +0200

    FullFilm casi completado

--- init.py
+++ init.py
@@ -40,7 +40,7 @@ def resultadoPeliculas(search,tipo,pelis):
                 mayor=cont
     for iter2 in pelis:
         if pelis_cont[iter2] == mayor:
-            pelisFin.append(Pelicula(iter2.titulo,iter2.precio,iter2.poster,iter2.imgfondo,iter2.director,iter2.estreno,iter2.desc))
+            pelisFin.append(Pelicula(iter2.titulo,iter2.precio,iter2.poster,iter2.imgfondo,iter2.director,iter2.estreno,iter2.desc,iter2.link))
     return pelisFin
 
 
@@ -87,7 +87,12 @@ def last():
 
 @app.route("/fullFilm/<name>")
 def fullFilm(name):
-    return checkSession("fullFilm.html")
+    json_url = os.path.join(SITE_ROOT, "data", "catalogo.json")
+    pelis=jsonAPelicula(json_url)
+    for peli in pelis:
+        if name == peli.link:
+            return checkSessionPelis("fullFilm.html",peli)
+    return redirect('/last')
 
 @app.route("/basket")
 def basket():
@@ -109,12 +114,12 @@ def search():
         search=request.args.get('search')
         tipo=request.args.get('programa')
 
-        return render_template("search.html",peliculas=resultadoPeliculas(search,tipo,pelis))
+        return checkSessionPelis("search.html",peliculas=resultadoPeliculas(search,tipo,pelis))
     elif request.method == 'POST':
         search=request.form['search']
         tipo=request.form['programa']
 
-        return render_template("search.html",peliculas=resultadoPeliculas(search,tipo,pelis))
+        return checkSessionPelis("search.html",peliculas=resultadoPeliculas(search,tipo,pelis))
     else:
         return redirect(url_for('last'))
 

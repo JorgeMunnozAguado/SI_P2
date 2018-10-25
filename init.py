4c26e15ff93e875d9ff45de0d169b709ae54ec50
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-10-25 11:51:26 +0200

    Some menor changes. And now you can enter to the pay web.

--- init.py
+++ init.py
@@ -1,11 +1,11 @@
 from flask import Flask, session, render_template, send_from_directory, request, redirect, make_response
-import datetime
 from pelicula import Pelicula
 from utilficheros import jsonAPelicula, resultadoPeliculas, searchFilms
 from users import Users
 import os
 import sys
 import json
+import time
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
@@ -42,19 +42,17 @@ def index():
 
             if Users.checkUser(session['username'], session['password']):
                 return redirect("/last")
+                
 
     if request.method == 'POST':
 
         username = request.form['username']
         password = request.form['password']
 
-        if Users.checkUser(username, password): #username == "admin" and password == "admin":
-
-            expire_date = datetime.datetime.now()
-            expire_date = expire_date + datetime.timedelta(hours = 1)
+        if Users.checkUser(username, password):
 
             resp = make_response(redirect("/last"))
-            #resp.set_cookie('SessionCookie', username, expires = expire_date)
+            
             session['username'] = username
             session['password'] = password
             
@@ -63,8 +61,8 @@ def index():
         else:
             return render_template("index-fail.html")
 
-    else:
-        return render_template("index.html")
+
+    return render_template("index.html")
             
 
 @app.route("/last")
@@ -73,38 +71,44 @@ def last():
     pelis = jsonAPelicula(json_url)
     return checkSessionPelis("last.html", pelis)
 
+
 @app.route("/fullFilm/<name>")
 def fullFilm(name):
+
     json_url = os.path.join(SITE_ROOT, "data", "catalogo.json")
-    pelis=jsonAPelicula(json_url)
+    pelis = jsonAPelicula(json_url)
+    
     for peli in pelis:
         if name == peli.link:
             return checkSessionPelis("fullFilm.html", peli)
             
     return redirect('/last')
 
+
 @app.route("/basket")
 def basket():
 
-    if "basket" in session and "SessionCookie" in request.cookies:
+    if "SessionCookie" in request.cookies:
+    
+        if "basket" in session:
 
-        obj = json.loads(session['basket'])
-            
-        buscar = obj["films"]
+            obj = json.loads(session['basket'])
+                
+            buscar = obj["films"]
 
-        json_url = os.path.join(SITE_ROOT, "data", "catalogo.json")
-        pelis = jsonAPelicula(json_url)
+            json_url = os.path.join(SITE_ROOT, "data", "catalogo.json")
+            pelis = jsonAPelicula(json_url)
 
-        ret = searchFilms(buscar, pelis)
+            ret = searchFilms(buscar, pelis)
 
-        return render_template("basket.html", films = ret[0], precioTotal = ret[1], user=session['username'])
+            return render_template("basket.html", films = ret[0], precioTotal = ret[1], user=session['username'])
 
-    elif "SessionCookie" in request.cookies:
-        return checkSession("basket.html")
-        
-    else:
-        return redirect("/")
-        
+        else:
+            return checkSession("basket.html")
+    
+    return redirect("/")
+
+
 @app.route("/pay", methods=['POST','GET'])
 def pay():
 
@@ -114,12 +118,20 @@ def pay():
 
             if Users.checkUser(session['username'], session['password']):
             
-        else:
-            return render_template("index.html")
+                user = Users.getUserFromDB(session['username'])
+                
+                return render_template("pay.html", user=user, date=time.strftime("%d/%m/%y"))
+            
+        
+            return render_template("pay.html")
+            
+    redirect("/")
+    
 
 @app.route("/history")
 def history():
     return checkSessionPelis("history.html", Users.listUserFilms(session['username']))
+
 
 @app.route("/sing_up")
 def registro():
@@ -184,15 +196,9 @@ def closeSession():
 
     if "SessionCookie" in request.cookies:
 
-        # Creo que no hay que hacer mas comprobaciones
-
-        resp = make_response(redirect("/"))
-        #resp.set_cookie('SessionCookie', '', expires = 0)
         session.clear()
-        return resp
 
-    else:
-        return redirect("/")
+    return redirect("/")
 
 
 @app.route("/ajax_url", methods=['POST'])
@@ -238,15 +244,18 @@ def send_styles(path):
 def send_scripts(path):
     return send_from_directory('templates/scripts', path)
 
+
 def checkSession(url):
 
     if "SessionCookie" in request.cookies:
+    
         if 'username' in session and 'password' in session:
 
             if Users.checkUser(session['username'], session['password']):
                 return render_template(url, user=session['username'])
                 
     return redirect("/")
+
 
 def checkSessionPelis(url, peliculas):
 
